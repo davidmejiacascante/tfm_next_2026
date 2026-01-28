3089a93269d5a156a82b71297b1ec8e49459b45e
David Mejia Cascante <david.mejia.cascante@gmail.com>
2026-01-28 12:29:37 -0600

    v1.17

--- A5.py
+++ A5.py
@@ -13,11 +13,12 @@ st.set_page_config(
 st.set_option("client.showSidebarNavigation", True)
 
 st.logo("img/logo.png")
-st.title('Asignatura 5.Fuentes y Obtención de Datos.')
-st.header('BURNOUTGUARD: Sistema De Detección Temprana De Riesgo De Burnout.')
+
 
 # Using "with" notation
 with st.sidebar:
+    st.title('Asignatura 5.Fuentes y Obtención de Datos.')
+    st.header('BURNOUTGUARD: Sistema De Detección Temprana De Riesgo De Burnout.')
     st.subheader('Profesaora:')
     st.text('Xisca Sorell')
     st.subheader('Integrantes:')
