b33a598ff7244f5eb44a9f97873b203260508463
David Mejia Cascante <david.mejia.cascante@gmail.com>
2026-01-28 12:15:00 -0600

    v1.11

--- A5.py
+++ A5.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="BurnoutGuard - Exploración inicial", layout="wide"
+    page_title="BurnoutGuard - Exploración inicial", layout="wide",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
