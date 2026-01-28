cc3b1b6d93a22e9e22e5498ab57163d72358b238
David Mejia Cascante <david.mejia.cascante@gmail.com>
2026-01-28 12:06:56 -0600

    layout updates 4

--- A5.py
+++ A5.py
@@ -2,14 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="BurnoutGuard - Exploración inicial", layout="wide")
 st.logo("img/logo.png")
-#st.title('Asignatura 5.Fuentes y Obtención de Datos.')
-#st.header('BURNOUTGUARD: SISTEMA DE DETECCIÓN TEMPRANA DE RIESGO DE BURNOUT')
-#st.subheader('Profesaora:')
-#st.text('Xisca Sorell')
-#st.subheader('Integrantes:')
-#st.text('David Mejía Cascante | Daniel Vargas Salazar | Juan Luis Chávez Mejía | María Cubero Sandoval | Melany Jiménez Nin')
 st.title('Asignatura 5.Fuentes y Obtención de Datos.')
-st.header('BURNOUTGUARD: SISTEMA DE DETECCIÓN TEMPRANA DE RIESGO DE BURNOUT')
+st.header('BURNOUTGUARD: Sistema De Detección Temprana De Riesgo De Burnout.')
 
 col1,col2,col3,col4 = st.columns(4)
 with col1:
@@ -17,13 +11,14 @@ with col1:
     st.text('Xisca Sorell')
     st.subheader('Integrantes:')
     st.text('David Mejía Cascante\nDaniel Vargas Salazar\nJuan Luis Chávez Mejía\nMaría Cubero Sandoval\nMelany Jiménez Nin')
+    st.subheader('Enero 2026')
 with col2:
     st.title("DASHBOARD")
     st.write("Explorador simple para el dataset procesado. Ejecutar después del Notebook 02.")
     data_path = Path("data_processed/burnoutguard_dataset.csv")
     if not data_path.exists():
         st.warning("No encuentro data_processed/burnoutguard_dataset.csv. Ejecuta notebooks/02_clean_prepare.ipynb primero.")
-        st.stop()
+        #st.stop()
 
     df = pd.read_csv(data_path)
 
