aea18ba5787669b7e9a79e4eb4f4fb53481aa9f7
David Mejia Cascante <david.mejia.cascante@gmail.com>
2026-01-31 12:27:12 -0600

    v3.1.1

--- main.py
+++ main.py
@@ -227,11 +227,11 @@ with tab2:
     st.header("A dog")
     st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
     st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-with tab3:
-    col1,col2 = st.columns(2)
-    with col1:
-        st.header("COL 1")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-    with col2:
-        st.header("COL 2")
-        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+#with tab3:
+    #col1,col2 = st.columns(2)
+    #with col1:
+        #st.header("COL 1")
+        #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+    #with col2:
+        #st.header("COL 2")
+        #st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
